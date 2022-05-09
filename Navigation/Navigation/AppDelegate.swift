63689a1ebea58b86104aee45af99a1307189e31c
Vladislav Kovalsky <kovalskyvk@icloud.com>
2022-05-09 11:36:31 +0700

    Homework-2.3.2
    Обе части задания выполнены сразу

---
+++ Navigation/Navigation/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  Navigation
+//
+//  Created by Владислав Ковальский on 20.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = setupTabBarController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+    
+    func setupFeedViewController() -> UINavigationController {
+        let feedViewController = FeedViewController()
+        feedViewController.tabBarItem.title = "Лента"
+        feedViewController.tabBarItem = UITabBarItem(title: "Лента", image: UIImage(systemName: "doc.richtext"), tag: 0)
+        return UINavigationController(rootViewController: feedViewController)
+    }
+    
+    func setupProfileViewController() -> UINavigationController {
+    let profileViewController = LogInViewController()
+    profileViewController.title = "Профиль"
+    profileViewController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person.circle"), tag: 1)
+    return UINavigationController(rootViewController: profileViewController)
+        
+    }
+    
+    func setupTabBarController() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        UITabBar.appearance().backgroundColor = .systemGray
+        tabBarController.viewControllers = [setupFeedViewController(), setupProfileViewController()]
+        return tabBarController
+    }
+
+
+}
+
+
